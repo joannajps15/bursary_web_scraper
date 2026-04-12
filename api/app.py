8148a75615082c4ad38a329d30c7d32a6ccd943d
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2026-04-12 15:03:53 -0400

    dotenv setup for environment variables

--- api/app.py
+++ api/app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, abort, send_file
 from flask_socketio import SocketIO
 from werkzeug.middleware.proxy_fix import ProxyFix
+from dotenv import load_dotenv
 
 from routes.create_award_info_tables import *
 from routes.scrape import *
@@ -14,6 +15,7 @@ cursor = None
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1, x_host=1)
 app.socketio = SocketIO(app, cors_allowed_origins="*")
+load_dotenv() 
 
 # --- API Calls ---
 
