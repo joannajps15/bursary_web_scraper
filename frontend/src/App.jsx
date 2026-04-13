09d8290ce8abba38268adbab39a0f6829fbfa8a1
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2026-04-13 01:34:15 -0400

    added https

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   });
   const excelDownloadURL = useRef(null);
   const tableRef = useRef(null);
-  const socket = io('http://15.222.61.136/ws')
+  const socket = io('http://16.54.51.53/ws')
   const [scrapeStatus, setScrapeStatus] = useState('false');
 
   const fetchAwards = async () => {
