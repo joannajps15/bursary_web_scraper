afa11f2fac24f9842b65f97679644d64f4b02a37
Joanna Sebastiampillai <j4sebast@uwaterloo.ca>
2026-04-13 20:35:20 -0400

    certbot configs setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   });
   const excelDownloadURL = useRef(null);
   const tableRef = useRef(null);
-  const socket = io('http://16.54.51.53/ws')
+  const socket = io('https://uwaterloobursary.app/ws')
   const [scrapeStatus, setScrapeStatus] = useState('false');
 
   const fetchAwards = async () => {
